808473ce86e8f85595a60005a0513558026b6bcb
abhi5733 <abhijeetkale5733@gmail.com>
2023-05-02 16:42:04 +0530

    add

--- routes/adminRoute.js
+++ routes/adminRoute.js
@@ -124,26 +124,24 @@ const jwt = require("jsonwebtoken")
  let rooms
   if(category.length>0 && location && type.length>0 ){
     rooms = await  hotelModel.find({category,location,type_of_room:type}).limit(10).skip(page)
- }else{
-    rooms = await hotelModel.find().limit(10).skip(page)
  }
-//  else if(category.length>0  && location ){
-//     rooms = await  hotelModel.find({category,location}).limit(10).skip(page)
-//  }else if(category.length>0  && type.length>0 ){
+ else if(category.length>0  && location ){
+    rooms = await  hotelModel.find({category,location}).limit(10).skip(page)
+ }else if(category.length>0  && type.length>0 ){
             
-//             rooms = await  hotelModel.find({category,type_of_room:type}).limit(10).skip(page)
+            rooms = await  hotelModel.find({category,type_of_room:type}).limit(10).skip(page)
              
-//     }else if(location && type.length>0 ){
-//             rooms = await hotelModel.find({location,type_of_room:type}).limit(10).skip(page)
-//     }else if(location.length>0 ){
-//         rooms = await  hotelModel.find({location}).limit(10).skip(page)
-//     }else if(type.length>0 ){
-//         rooms = await  hotelModel.find({type_of_room:type }).limit(10).skip(page)
-//     }else if(category.length>0 ){
-//         rooms = await  hotelModel.find({category:query.category}).limit(10).skip(page)
-//     }else{
-//              rooms = await hotelModel.find().limit(10).skip(page)
-//          }
+    }else if(location && type.length>0 ){
+            rooms = await hotelModel.find({location,type_of_room:type}).limit(10).skip(page)
+    }else if(location.length>0 ){
+        rooms = await  hotelModel.find({location}).limit(10).skip(page)
+    }else if(type.length>0 ){
+        rooms = await  hotelModel.find({type_of_room:type }).limit(10).skip(page)
+    }else if(category.length>0 ){
+        rooms = await  hotelModel.find({category:query.category}).limit(10).skip(page)
+    }else{
+             rooms = await hotelModel.find().limit(10).skip(page)
+         }
   
      res.send(rooms)
 // res.send(location,category,type)
