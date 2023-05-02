578d8ea21d2333b8c4a5a2320f70845ce7a5989d
abhi5733 <abhijeetkale5733@gmail.com>
2023-05-02 16:17:58 +0530

    add

--- routes/adminRoute.js
+++ routes/adminRoute.js
@@ -122,22 +122,22 @@ const jwt = require("jsonwebtoken")
 
     try{
  let rooms
- if(category.length>0 && location!=="" && type.length>0 ){
+ if(category && location && type){
     rooms = await  hotelModel.find({category,location,type_of_room:type}).limit(10).skip(page)
  }
- else if(category.length>0 && location!=="" ){
+ else if(category && location ){
     rooms = await  hotelModel.find({category,location}).limit(10).skip(page)
- }else if(category.length>0 && type.length>0 ){
+ }else if(category && type){
             
             rooms = await  hotelModel.find({category,type_of_room:type}).limit(10).skip(page)
              
-    }else if(location!=="" && type.length>0 ){
+    }else if(location && type){
             rooms = await hotelModel.find({location,type_of_room:type}).limit(10).skip(page)
-    }else if(location!==""){
+    }else if(query.location){
         rooms = await  hotelModel.find({location}).limit(10).skip(page)
-    }else if(type.length>0 ){
+    }else if(type){
         rooms = await  hotelModel.find({type_of_room:type }).limit(10).skip(page)
-    }else if(category.length>0){
+    }else if(query.category){
         rooms = await  hotelModel.find({category:query.category}).limit(10).skip(page)
     }else{
              rooms = await hotelModel.find().limit(10).skip(page)
